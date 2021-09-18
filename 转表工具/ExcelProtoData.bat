8624ffd60c8a04bd2e6527c46031d00499059047
stephenhou2 <stephenhou2@163.com>
2021-09-19 02:18:17 +0800

    更新

--- 转表工具/ExcelProtoData.bat
+++ 转表工具/ExcelProtoData.bat
@@ -1,6 +1,11 @@
 @echo off
 
-protoc --csharp_out=./output ./output/TableData.proto
+for %%i in (./output/*.proto) do (
+protoc --csharp_out=./output ./output/%%i
+)
+
+::protoc --csharp_out=./output ./output/TableData.proto
 echo proto文件生成成功
 
 copy .\output\TableData.cs ..\Assets\Scripts\Proto
+pause
