f3c5ad23f4780ac269b3788774422c5b143d0ef7
stephenhou2 <stephenhou2@163.com>
2021-10-14 14:32:21 +0800

    bittype 调整

--- WDGame/Assets/Scripts/Agent/Agent.cs
+++ WDGame/Assets/Scripts/Agent/Agent.cs
@@ -9,12 +9,5 @@ public abstract class Agent : IAgent
 
 public class Building:Agent
 {
-    BitType state = BitType.BindBitTypes(new List<BitType>
-    {
-        AgentStateDefine.INTERACT_FLAG,
-        AgentStateDefine.MAGIC_IMMUNE_FLAG,
-        AgentStateDefine.PHYSICAL_IMMUNE_FLAG,
-        AgentStateDefine.MAGIC_FLAG,
-        AgentStateDefine.VISIBLE_FLAG,
-    });
+
 }
